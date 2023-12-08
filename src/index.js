bc445288fa1504c40b2e00ea8e0e539c9aaf361d
andersonbarbosas <anderson.nandinho@hotmail.com>
2023-12-08 20:02:53 -0300

    Inicio do Projeto

---
+++ src/index.js
@@ -0,0 +1,34 @@
+console.log("This is the Chalanger Of Hero - DIO")
+
+console.log("Our Hero, currently is on the **Fase 4** the he journey")
+console.log("Let's check your rating together?")
+
+let password = 5999
+console.log(password)
+
+if (password == 1000){
+        console.log("Ferro");
+
+    } else if (password >= 1001 && password <= 2000) {
+        console.log("Bronze");
+    } else if (password >= 2001 && password <= 5000) {
+        console.log("Prata");
+    } else if (password >= 5001 && password <= 7000) {
+        console.log("Ouro");
+    } else if (password >= 7001 && password <= 8000) {
+        console.log("Platina");
+    } else if (password >= 8001 && password <= 9000) {
+        console.log("Ascendente");
+    } else if (password >= 9001 && password <= 10000) {
+        console.log("Imortal");
+    } else if (password >= 10001) {
+        console.log("Radiante");
+}
+
+
+function UserName(){
+    console.log()
+
+
+
+}
