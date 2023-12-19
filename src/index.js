08becb44e33ac5082fd258c6968cb2e1d9403718
andersonbarbosas <anderson.nandinho@hotmail.com>
2023-12-18 21:31:21 -0300

    Atualização

--- src/index.js
+++ src/index.js
@@ -15,36 +15,28 @@ diaMon = mon.getMonth() + 1
 
 console.log()
 let password = (diaSec * diaMon + diaHou + diaSem * (diaSec * diaSec))
-console.log(`The Level of Our Hero DIO is: ${password}XP`)
+console.log(`The Level of Our Hero DIO is: ${password}xp`)
 console.log()
 console.log("And with this Level, Your Sword is of the Class:")
 if (password<= 1000){
-        console.log("Ferro");
+        console.log("Iron");
     } else if (password >= 1001 && password <= 2000) {
-        console.log("Bronze");
+        console.log("Copper");
     } else if (password >= 2001 && password <= 5000) {
-        console.log("Prata");
+        console.log("Silver");
     } else if (password >= 5001 && password <= 7000) {
-        console.log("Ouro");
+        console.log("Gold");
     } else if (password >= 7001 && password <= 8000) {
-        console.log("Platina");
+        console.log("Platinum");
     } else if (password >= 8001 && password <= 9000) {
-        console.log("Ascendente");
+        console.log("Ascending");
     } else if (password >= 9001 && password <= 10000) {
-        console.log("Imortal");
+        console.log("Immortal");
     } else if (password >= 10001) {
-        console.log("Radiante");
+        console.log("Radiant");
 }
 console.log()
-console.log("Agradeçemos pelos seus Serviços Heroicos. Thank Youuu!!!")
+console.log("Let us thank you for your Heroic Services. Thank Youu!")
 console.log()
-console.log("E se você tentasse mais uma vez?")
+console.log("And if you try again?")
 
-
-
-/*
-console.log(diaSem)
-console.log(diaHou)
-console.log(diaMon)
-console.log(diaSec)
-*/
